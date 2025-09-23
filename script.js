ed9ef414b117283e4d01c1f66d9ef5dc6994972d
romkly <main.romanklymenko@gmail.com>
2025-09-23 10:33:59 +0200

    feat: implement basic Todo List app with add and delete functionality - Added HTML structure with input for todo name and due date - Implemented addTodo() to create new todo items with name + date - Implemented renderTodoList() to display todos dynamically - Added delete button for each todo item to remove it from the list - Integrated simple state management using an in-memory array

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 const todoList = [];
 
-function addTodo() {
-    const inputElement = document.querySelector('js-name-input');
-    const name = inputElement.value
-    console.log(name);
+renderTodoList();
+
+function renderTodoList() {
+        let todoListHTML = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate; OR:
+        const { name, dueDate } = todoObject;
+        const html = `
+            <p>
+                ${name} - ${dueDate}
+                <button onclick="
+                    todoList.splice(${i}, 1);
+                    renderTodoList();
+                ">Delete</button>
+            </p>`;
+        todoListHTML += html;
+    }
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+
+    const dueDateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dueDateInputElement.value;
+    
+    todoList.push({ name, dueDate });
+    // OR: todoList.push({ name: name, dueDate: dueDate });
+    inputElement.value = '';
+    renderTodoList();
+}
+
